a46f600542e968c729c31d4c232de7a2f6001d98
joshuacke <joshuacke@gmail.com>
2018-10-23 19:33:52 -0500

    fixed libraries

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base 
-from sqlalchemy.orm import relationship
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from sqlalchemy.orm import relationship,sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -9,17 +10,17 @@ db = SQLAlchemy()
 
 class Books(Base):
 	__tablename__ = 'books'
-	google_id = db.Column(db.String(), primary_key = true, nullable = False)
+	google_id = db.Column(db.String(), primary_key = True, nullable = False)
 	title = db.Column(db.String(), unique=True, nullable = False)
 	publication_date = db.Column(db.String(), nullable = False)
 	image_url = db.Column(db.String(), nullable = False)
 	description = db.Column(db.String(), nullable = False)
 	digest = db.Column(db.String(), nullable = False)
 
-class Authors(db.Model):
+class Authors(Base):
     __tablename__ = 'authors'
     born = db.Column(db.String(), nullable = False)
-    name = db.Colunn(db.String(), primary_key = true, nullable = False)
+    name = db.Column(db.String(), primary_key = True, nullable = False)
     education = db.Column(db.String(), nullable = False)
     nationality = db.Column(db.String(), nullable = False)
     description = db.Column(db.String(), nullable = False)
@@ -30,15 +31,15 @@ class Authors(db.Model):
         return self.name
 
 class Publishers(Base):
-	__tablename__ = 'publishers'
-	name = db.Column(db.String(), primary_key = true, nullable = False)
-	wikipedia_url = db.Column(db.String(), nullable = False)
-	description = db.Column(db.String(), nullable = False)
-	owner = db.Column(db.String(), nullable = False)
-	founded = db.Column(db.String(), nullable = False)
-	location = db.Column(db.String(), nullable = False)
-	image_url = db.Column(db.String(), nullable = False)
-	website = db.Column(db.String(), nullable = False)
+    __tablename__ = 'publishers'
+    name = db.Column(db.String(), primary_key = True, nullable = False)
+    wikipedia_url = db.Column(db.String(), nullable = False)
+    description = db.Column(db.String(), nullable = False)
+    owner = db.Column(db.String(), nullable = False)
+    founded = db.Column(db.String(), nullable = False)
+    location = db.Column(db.String(), nullable = False)
+    image_url = db.Column(db.String(), nullable = False)
+    website = db.Column(db.String(), nullable = False)
 
-	def __repr__(self):
-		return self.name
+    def __repr__(self):
+        return self.name

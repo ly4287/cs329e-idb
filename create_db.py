9b88964438571390fe76570f639ec17e6eb17835
joshuacke <joshuacke@gmail.com>
2018-11-08 16:47:29 -0600

    testing

--- create_db.py
+++ create_db.py
@@ -47,80 +47,80 @@ def create_books():
         for j in authors_attribute:
             author = j['name']
         newBook = Book(title = title, google_id = g_id, isbn = isbn_, image_url = img_url, publication_date = pub_date, description = desc, author = author, publisher = publisher)
-	cursor.execute("SELECT google_id FROM book")
-	rows = cursor.fetchall()
-	for row in rows:
-		if g_id in row:
-			dupe = True
-			continue
-	if(not dupe):
-        	# After I create the book, I can then add it to my session. 
-        	db.session.add(newBook)
-        	# commit the session to my DB.
-        	db.session.commit()
-        	counter += 1
-		print("we added a book into the DB! it was: "+title)
-	else:
-		print("there was a dupe!")
+        cursor.execute("SELECT google_id FROM book")
+        cursor.fetchall()
+	# for row in rows:
+	# 	if g_id in row:
+	# 		dupe = True
+	# 		continue
+	# if(not dupe):
+    	# After I create the book, I can then add it to my session. 
+        db.session.add(newBook)
+    	# commit the session to my DB.
+        db.session.commit()
+        counter += 1
+ #        print("we added a book into the DB! it was: "+title)
+	# else:
+	# 	print("there was a dupe!")
 
-def create_authors():
-    book = load_json('books.json')
-    counter = 0
-    dupe = False
-    for oneBook in book:
-        title = oneBook['title']
-        publishers_attribute = oneBook['publishers']
-        for i in publishers_attribute:
-            publisher = i['name']
-        authors_attribute = oneBook['authors']
-        for j in authors_attribute:
-            author = j['name']
-            try:
-                born = j['born']
-            except:
-                born = "None"
-            try:
-                education = j['education']
-            except:
-                education = "None"
-            try:
-                nationality = j['nationality']
-            except:
-                nationality = "None"
-            try:
-                desc = j['description']
-            except:
-                desc = "None"
-            try:
-                alma = j['alma_mater']
-            except:
-                alma = "None"
-            try:
-                wiki = j['wikipedia_url']
-            except:
-                wiki = "None"
-            try: 
-                died = j['died']
-            except:
-                died = "None"
-            try:
-                img_url = j['image_url']
-            except:
-                img_url = "None"
-            newAuthor =  Author(name = author, born = born, education = education, nationality = nationality, description = desc, alma_mater = alma, wikipedia_url = wiki, image_url = img_url, publisher = publisher)
-	    cursor.execute("SELECT name FROM author")
-	    rows = cursor.fetchall()
-	    for row in rows:
-		if author in row:
-			dupe = True
-			continue
-	    if(not dupe):
-		db.session.add(newAuthor)
-		db.session.commit()
-		counter += 1
-		print("we added an author into the DB! it was: "+author)
-	    else:
-		print("there was a dupe!")
+# def create_authors():
+#     book = load_json('books.json')
+#     counter = 0
+#     dupe = False
+#     for oneBook in book:
+#         title = oneBook['title']
+#         publishers_attribute = oneBook['publishers']
+#         for i in publishers_attribute:
+#             publisher = i['name']
+#         authors_attribute = oneBook['authors']
+#         for j in authors_attribute:
+#             author = j['name']
+#             try:
+#                 born = j['born']
+#             except:
+#                 born = "None"
+#             try:
+#                 education = j['education']
+#             except:
+#                 education = "None"
+#             try:
+#                 nationality = j['nationality']
+#             except:
+#                 nationality = "None"
+#             try:
+#                 desc = j['description']
+#             except:
+#                 desc = "None"
+#             try:
+#                 alma = j['alma_mater']
+#             except:
+#                 alma = "None"
+#             try:
+#                 wiki = j['wikipedia_url']
+#             except:
+#                 wiki = "None"
+#             try: 
+#                 died = j['died']
+#             except:
+#                 died = "None"
+#             try:
+#                 img_url = j['image_url']
+#             except:
+#                 img_url = "None"
+#             newAuthor =  Author(name = author, born = born, education = education, nationality = nationality, description = desc, alma_mater = alma, wikipedia_url = wiki, image_url = img_url, publisher = publisher)
+# 	    cursor.execute("SELECT name FROM author")
+# 	    rows = cursor.fetchall()
+# 	    for row in rows:
+# 		if author in row:
+# 			dupe = True
+# 			continue
+# 	    if(not dupe):
+# 		db.session.add(newAuthor)
+# 		db.session.commit()
+# 		counter += 1
+# 		print("we added an author into the DB! it was: "+author)
+# 	    else:
+# 		print("there was a dupe!")
 '''
         if session.query(Authors).filter(Authors.name == newAuthor.name).count() == 0: 
             db.session.add(newAuthor)

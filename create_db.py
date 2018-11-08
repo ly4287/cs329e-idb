6998f8c6fee19a2052e7383e446ac2caa79f83c5
Patrick Zhu <patticus.zhu@gmail.com>
2018-11-08 23:42:03 +0000

    done?

--- create_db.py
+++ create_db.py
@@ -108,22 +108,21 @@ def create_authors():
                 img_url = j['image_url']
             except:
                 img_url = "None"
-        newAuthor =  Author(name = author, born = born, education = education, nationality = nationality, description = desc, alma_mater = alma, wikipedia_url = wiki, image_url = img_url, publisher = publisher)
-	cursor.execute("SELECT name FROM author")
-	rows = cursor.fetchall()
-	for row in rows:
-		if author in row:
-			dupe = True
-			continue
-	if(not dupe):
-		db.session.add(newAuthor)
-		db.session.commit()
-		counter += 1
-		print("we added an author into the DB! it was: "+author)
-	else:
-		print("there was a dupe!")
-        dupe = False
-        #
+            newAuthor =  Author(name = author, born = born, education = education, nationality = nationality, description = desc, alma_mater = alma, wikipedia_url = wiki, image_url = img_url, publisher = publisher)
+	    cursor.execute("SELECT name FROM author")
+	    rows = cursor.fetchall()
+	    for row in rows:
+		    if author in row:
+			    dupe = True
+			    continue
+	    if(not dupe):
+		    db.session.add(newAuthor)
+		    db.session.commit()
+		    counter += 1
+		    print("we added an author into the DB! it was: "+author)
+	    else:
+		    print("there was a dupe!")
+		    dupe = False
 '''
         if session.query(Authors).filter(Authors.name == newAuthor.name).count() == 0: 
             db.session.add(newAuthor)

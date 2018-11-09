623c245edfbe64502320900973cacf5a3fbf2791
Patrick Zhu <patticus.zhu@gmail.com>
2018-11-09 03:40:22 +0000

    tests done

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ cursor = conn.cursor()
 
 class DBTestCases(unittest.TestCase):
     def test_source_insert_1(self):
-        s = Book(id='5000', title = 'TESTONE')
+        s = Book(id='5000', title = 'josh')
         db.session.add(s)
         db.session.commit()
 
@@ -16,49 +16,65 @@ class DBTestCases(unittest.TestCase):
         rows = cursor.fetchall()
         swag = ''
         for row in rows:
-            if(row[1] == 'TESTONE'):
+            if(row[1] == 'josh'):
                 swag = row[1]
                 break
-        self.assertEqual(swag, 'TESTONE')
+        self.assertEqual(swag, 'josh')
 
         db.session.delete(s)
         db.session.commit()
 
-    def test_db_1(self):
-        r = db.session.query(Books).filter_by(google_id = 'wrOQLV6xB-wC').one()
-        self.assertEqual(str(r.google_id), 'wrOQLV6xB-wC')
-    def test_db_2(self):
-        r = db.session.query(Books).filter_by(google_id = 'kPpeTrfXpKsC').first()
-        self.assertEqual(str(r.google_id), 'kPpeTrfXpKsC')
-    def test_db_3(self):
-        r = db.session.query(Books).filter_by(isbn = '1101486406').first()
-        self.assertEqual(str(r.isbn), '1101486406')
-    def test_db_4(self):
-        r = db.session.query(Books).filter_by(title = 'Royal Assassin').first()
-        self.assertEqual(str(r.Books), 'Royal Assassin')
-    def test_db_5(self):
-        r = db.session.query(Authors).filter_by(name = 'Terry Goodkind').first()
-        self.assertEqual(str(r.name), 'Terry Goodkind')
-    def test_db_6(self):
-        r = db.session.query(Authors).filter_by(name = 'Stephen King').first()
-        self.assertEqual(str(r.name), 'Stephen King')
-    def test_db_7(self):
-        r = db.session.query(Authors).filter_by(education = 'Dana Hall School, 1928').first()
-        self.assertEqual(str(r.education), 'Dana Hall School, 1928')
-    def test_db_8(self):
-        r = db.session.query(Authors).filter_by(alma_mater = 'Princeton University').first()
-        self.assertEqual(str(r.alma_mater), 'Princeton University')
-    def test_db_9(self):
-        r = db.session.query(Publishers).filter_by(name = 'Del Rey Books').first()
-        self.assertEqual(str(r.name), 'Del Rey Books')
-    def test_db_10(self):
-        r = db.session.query(Publishers).filter_by(owner = 'Scholastic Corporation').first()
-        self.assertEqual(str(r.owner), 'Scholastic Corporation')
-    def test_db_11(self):
-        r = db.session.query(Publishers).filter_by(owner = 'Penguin Random House').first()
-        self.assertEqual(str(r.owner), 'Penguin Random House')
-    def test_db_12(self):
-        r = db.session.query(Publishers).filter_by(name = 'Palgrave Macmillan').first()
-        self.assertEqual(str(r.name), 'Palgrave Macmillan')
+    def test_source_insert_2(self):
+        s = Book(id='5001', title = 'paul')
+        db.session.add(s)
+        db.session.commit()
+
+        cursor.execute("SELECT id, title FROM Book")
+        rows = cursor.fetchall()
+        swag = ''
+        for row in rows:
+            if(row[1] == 'paul'):
+                swag = row[1]
+                break
+        self.assertEqual(swag, 'paul')
+
+        db.session.delete(s)
+        db.session.commit()
+
+    def test_source_insert_3(self):
+	s = Author(id='6969', name = 'bam')
+	db.session.add(s)
+	db.session.commit()
+	
+	cursor.execute("SELECT id, name FROM Author")
+	rows = cursor.fetchall()
+	swag = ''
+	for row in rows:
+		if(row[1] == 'bam'):
+			swag = row[1]
+			break
+	self.assertEqual(swag, 'bam')
+
+	db.session.delete(s)
+	db.session.commit()
+
+    def test_source_insert_4(self):
+	s = Publisher(id='99100', name = 'bum')
+	db.session.add(s)
+	db.session.commit()
+
+	cursor.execute("SELECT id, name FROM Publisher")
+	rows = cursor.fetchall()
+	swag = ''
+	for row in rows:
+		if(row[1] == 'bum'):
+			swag = row[1]
+			break
+	self.assertEqual(swag, 'bum')
+
+	db.session.delete(s)
+	db.session.commit()
+	
+
 if __name__ == '__main__':
 	unittest.main()
